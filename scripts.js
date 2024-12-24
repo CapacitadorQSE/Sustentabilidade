491df894b7730e6aba5ab034165b069a70a5845b
Capacitador <capacitadorqse@gmail.com>
2024-12-24 15:13:02 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,15 +3,17 @@ const content = document.getElementById("content");
 const increaseFont = document.getElementById("increase-font");
 const decreaseFont = document.getElementById("decrease-font");
 
-let currentFontSize = 16;
+let currentFontSize = 16; // Tamanho inicial da fonte
 
+// Aumentar Fonte
 increaseFont.addEventListener("click", () => {
   currentFontSize += 2;
   content.style.fontSize = `${currentFontSize}px`;
 });
 
+// Reduzir Fonte
 decreaseFont.addEventListener("click", () => {
-  if (currentFontSize > 10) {
+  if (currentFontSize > 12) {
     currentFontSize -= 2;
     content.style.fontSize = `${currentFontSize}px`;
   }
